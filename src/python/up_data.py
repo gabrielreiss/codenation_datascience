38b4c2c23116123a6a3a7356f6b7729527b3bb20
gabrielreiss <gabrielreissdecastro@gmail.com>
2020-06-11 00:21:21 -0300

    0.1

---
+++ src/python/up_data.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+import sqlalchemy
+
+BASE_DIR = "F:\Aprendendo python\CODENATION_Projeto_Final"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+
